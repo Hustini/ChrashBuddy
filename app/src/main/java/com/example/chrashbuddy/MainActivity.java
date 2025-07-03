21d2d9d2081765a6816c8ce9148a01b119949e69
Hustini <ejdersalih02@gmail.com>
2025-07-03 13:23:40 +0200

    able to toogle the text of the button and the detection status

--- app/src/main/java/com/example/chrashbuddy/MainActivity.java
+++ app/src/main/java/com/example/chrashbuddy/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.chrashbuddy;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -31,6 +37,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 var intent = new Intent(MainActivity.this, AlarmActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        var detectionStatusSymbol = (ImageView)findViewById(R.id.detectionStatusSymbol);
+        final int[] currentColor = {Color.RED};
+        var activationToggleButton = (TextView)findViewById(R.id.activationToggleButton);
+        activationToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GradientDrawable drawable = (GradientDrawable) detectionStatusSymbol.getDrawable();
+                if (currentColor[0] == Color.RED) {
+                    drawable.setColor(Color.GREEN);
+                    currentColor[0] = Color.GREEN;
+                    activationToggleButton.setText("Deactivate detection");
+                } else {
+                    drawable.setColor(Color.RED);
+                    currentColor[0] = Color.RED;
+                    activationToggleButton.setText("Activate Detection");
+                }
             }
         });
     }
